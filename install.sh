2fd85c55ec9793e91aeed10767100f113bb0fcd0
German Ilyin <germanilyin@gmail.com>
2014-03-01 18:22:01 +0600

    install vundle

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/tmux.conf ~/.tmux.conf
-git submodule init
-git submodule udpate
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/NickLaMuro/vim-files.git ~/.vim/vim-files
 
 ln -s ~/.vim/vim-files/colors ~/.vim/colors
